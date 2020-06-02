83563385de7ff0e9af101474118f30f8ec6a6879
fxalban08 <francisco.alban@epn.edu.ec>
2020-06-02 18:31:05 -0500

    The prototype of the function Sum_Values has been added

--- Sum_Numbers_Until_Input.cpp
+++ Sum_Numbers_Until_Input.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int sumValues(int x);
+
 int main() {
 	int n;
 	cout<<"Enter the number which will be the last one to add to the sum of all numbers below it: "<<flush;		
 	cin>>n;
+
 	return 0;
 }
 
